b4073e7134b2926d365a70ec7388cb9bf5c9d457
Mert71 <31502152+Mert71@users.noreply.github.com>
2019-03-29 09:15:31 +0100

    Revert "Pokemonphp Added"
    This reverts commit 70958a2f642e1deb2e74dc7c4d6cc7151a2e6c1a.

--- Pokemon.php
+++ Pokemon.php
@@ -1,7 +1,7 @@
 <?php
 
 abstract class Pokemon {
-
+  
   public $EnergyType;
   public $Name;
   public $Hitpoints;
@@ -10,29 +10,21 @@ abstract class Pokemon {
   public $Weakness;
   public $Resistance;
 
-  public function __construct($nm, $healthp, $hitp, $et)
-  {
-      $this->Name = $nm;
-      $this->EnergyType = $et;
-      $this->Health = $healthp;
-      $this->Hitpoints = $hitp;
-      $this->Attack = [];
+  public function __construct($Name, $Hitpoints, $Energytype) {
+    $this->Name = $Name;
+  }
+}
+
+class Charmeleon extends Pokemon {
+
+  // setup attacks, name, health
+  function __construct() {
+
+    parent::__construct("Charmeleon", 100, "Fire" );
+
+    $this->Attacks[] = new Attack();
+    $this->Attacks[] = new Attack();
   }
 
-  function DoAttack($pokemon, $pokemon2, $target)
-  {
-      echo $this->Name . ' Will attack ' . $target->Name . ' Using ' . $this->Attack[0]->Name;
-      echo '<br>';
-      if ($pokemon->EnergyType == $pokemon2->Weakness->EnergyType) {
-          $pokemon2->Health = $pokemon2->Health - ($pokemon->Attack[0]->AttackPoints * $pokemon2->Weakness->Multiplier);
-      } else {
-          $pokemon2->Health = $pokemon2->Health - $pokemon->Attack[0]->AttackPoints;
-      }
 
-      if ($pokemon2->Health < 1) {
-          echo $pokemon2->Name . " Has Died ";
-      } else {
-          echo $pokemon2->Name . " Now Has " . $pokemon2->Health . " HP Left";
-      }
-  }
 }
